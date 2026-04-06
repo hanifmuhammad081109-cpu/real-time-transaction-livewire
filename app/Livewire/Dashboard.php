b21d2470ec53941bf232047e17182dbfa839aaf0
hanifmuhammad081109-cpu <hanifmuhammad081109@gmail.com>
2026-04-06 10:18:32 +0200

    Data Binding dan State Management

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -7,6 +7,8 @@ use Livewire\Component;
 class Dashboard extends Component
 {
     public $name = 'Ndoro';
+    public $asset = 15000000;
+    public $transactionCount = 0;
     public function render()
     {
         return view('livewire.dashboard');
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -8,23 +8,35 @@
     </div>
     <div class="">
         <h1 class="text-3xl font-black text-slate-800 tracking-tight">
-            Selamat Datang, <span class="text-indigo-600">{{ $name }}</span>
-            <p class="text-slate-400 mt-1 font-medium italic">Langkah kecil hari ini, dashboard besar esok hari.</p>
+            Selamat Datang , <span class="text-indigo-600">{{ $name }} !</span>
+            <p class="text-slate-400 mt-1 font-medium italic">Ubah profilmu secara real-time disini.</p>
         </h1>
+    </div>
+    <div class="flex flex-col gap-2 mt-5 mx-4">
+        <label class="text-xs font-bold text-slate-600 uppercase tracking-widest" for="name">Ganti nama:</label>
+        <input 
+            type="text" 
+            name="name"
+            id="name" 
+            class="px-4 py-2 rounded-xl border-2 border-slate-100 focus:border-indigo-500 focus:ring-0 outline-none trasition-all text-slate-700 font-medium" 
+            wire:model.live="name"
+            placeholder="Masukkan nama baru"
+        >
+        
     </div>
 
     <div class="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
         <div class="p-8 bg-slate-50 rounded-2xl border border-slate-300 transition-hover hover:border-indigo-700">
             <p class="text-slate-400 text-xs font-bold uppercase tracking-widest">Total Aset</p>
-            <p class="text-3xl font-black text-slate-800 mt-2">Rp.0</p>
+            <p class="text-3xl font-black text-slate-800 mt-2">Rp.{{ number_format($asset, 0, ',', '.') }}</p>
         </div>
          <div class="p-8 bg-slate-50 rounded-2xl border border-slate-300 transition-hover hover:border-indigo-700">
             <p class="text-slate-400 text-xs font-bold uppercase tracking-widest">Transaksi</p>
-            <p class="text-3xl font-black text-slate-800 mt-2">0</p>
+            <p class="text-3xl font-black text-slate-800 mt-2">{{ $transactionCount }}</p>
         </div>
          <div class="p-8 bg-indigo-600 rounded-2xl border border-indigo-500 text-white shadow-xl shadow-indigo-200">
             <p class="text-indigo-100 text-xs font-bold uppercase tracking-widest text-opacity-70">Progress Belajar</p>
-            <p class="text-3xl font-black mt-2">Bab 1 Selesai</p>
+            <p class="text-3xl font-black mt-2">Bab 2 : Data Binding</p>
         </div>
         
     </div>
