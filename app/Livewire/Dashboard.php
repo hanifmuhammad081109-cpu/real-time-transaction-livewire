151c9af3b858185c1ed7a50e25baab2ea507fedc
hanifmuhammad081109-cpu <hanifmuhammad081109@gmail.com>
2026-04-07 10:18:28 +0200

    Form & Validasi

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -9,6 +9,9 @@ class Dashboard extends Component
     public $name = 'Ndoro';
     public $asset = 15000000;
     public $transactionCount = 0;
+
+    #[Validate('required|numeric|min:10000', message: 'Minimum 10.000')]
+    public $nominal;
     public function plusAsset()
     {
         $this->asset += 1000000;
@@ -20,6 +23,16 @@ class Dashboard extends Component
             $this->asset -= 1000000;
             $this->transactionCount++;
         }
+    }
+    public function topUp()
+    {
+        //Menjalankan validasi berdasarkan #[Validate]
+        $this->validate();
+
+        $this->asset += $this->nominal;
+        $this->transactionCount++;
+
+        $this->reset('nominal');
     }
     public function render()
     {
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -51,8 +51,30 @@
         </div>
          <div class="p-8 bg-indigo-600 rounded-2xl border border-indigo-500 text-white shadow-xl shadow-indigo-200">
             <p class="text-indigo-100 text-xs font-bold uppercase tracking-widest text-opacity-70">Progress Belajar</p>
-            <p class="text-3xl font-black mt-2">Bab 3 : Action & Method</p>
+            <p class="text-3xl font-black mt-2">Bab 4 : Form & Validasi</p>
         </div>
-        
+    </div>
+
+    <div class="mt-12 p-10 bg-slate-900 rounded-3xl text-white shadow-2xl">
+        <h2 class="text-xl font-bold mb-6 flex items-center gap-2">
+            🌩️ Top Up Kostum
+        </h2>
+        <form wire:submit="topUp" class="flex flex-col md:flex-row gap-6 items-end">
+            <input 
+            type="number"
+            wire:model="nominal"
+            class="bg-slate-800 border-none rounded-xl px-6 py-4 text-xl font-bold text-indigo-300 focus:ring-2 focus:ring-indigo-500 outline-none trasition-all placeholder:text-slate-700"
+            placeholder="Minimal Top Up 10.000"
+            >
+
+            <button
+            wire:loading.attr="disabled"
+            type="submit"
+            class="w-full md:w-auto bg-indigo-600 hover:bg-indigo-500 px-10 py-4 rounded-xl font-black tracking-widest shadow-lg shadow-indigo-900/50 transition-all active:scale-95 disabled:opacity-50"
+            >Top Up</button>
+            @error('nominal')
+                <span class="text-red-400 text-xs mt-1 font-bold tracking-tight">⚠️ {{ $message }}</span>
+            @enderror
+        </form>
     </div>
 </div>
